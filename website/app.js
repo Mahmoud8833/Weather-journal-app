f96a6828eb0764436350cbb74fc796cb0f220aa4
Mahmoud Abd alaziz <mahmoud.a.ali.88@gmail.com>
2022-09-28 03:48:34 +0200

    Update multiple files
    *Install express.
    *Update server.js.
    *Update app.js.
    *Update style.css.

--- website/app.js
+++ website/app.js
@@ -1,5 +1,16 @@
 /* Global Variables */
+/* This is the API key and URL for the OpenWeatherMap API. */
+const apiKey = "abca8acf5deb3392f8c67a21434fa83a&units=imperial";
+const baseURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+async function getDate(url = "") {
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data.list);
+  return response;
+}
+getDate(baseURL);
