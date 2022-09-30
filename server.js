6b64f4ceefaa918f5f492ba15f2ed7016de3ce21
Mahmoud Abd alaziz <mahmoud.a.ali.88@gmail.com>
2022-09-30 22:15:15 +0200

    Add and Modify multiple files
    *Modify server.js.
    *Modify app.js.
    *Modify styles.css.
    *Add noaa-kcvlb727mn8-unsplash.jpg.

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
-// Setup empty JS object to act as endpoint for all routes
-const projectData = {};
-
 // Require Express to run server and routes
 const express = require("express");
+/* BodyParser is a middleware that parses the body of the request. */
 const bodyParser = require("body-parser");
+/* A middleware that allows cross-origin requests. */
 const cors = require("cors");
 
-// Start up an instance of app
+/* Creating an instance of the express function. */
 const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* A middleware that parses the body of the request. */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+/* It allows cross-origin requests. */
 app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static("website"));
 
+let projectData = {};
+
+app.get("/", (req, res) => {
+  res.send(projectData);
+});
+
+app.post("/", addData);
+
+function addData(req, res) {
+  projectData = req.body;
+  res.send(projectData);
+  console.log(projectData);
+}
+
 // Setup Server
-const localhost = "127.0.0.1";
-const port = 3000;
+let port = 3000;
+localhost = "127.0.0.1";
+
 app.listen(port, () => {
   console.log(`Server is running on http://${localhost}:${port}`);
 });
--- website/app.js
+++ website/app.js
@@ -1,16 +1,55 @@
 /* Global Variables */
-/* This is the API key and URL for the OpenWeatherMap API. */
-const apiKey = "abca8acf5deb3392f8c67a21434fa83a&units=imperial";
-const baseURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`;
+const apiKey = "&appid=abca8acf5deb3392f8c67a21434fa83a&units=imperial";
+const baseUrl = `https://api.openweathermap.org/data/2.5/weather?zip=`;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+let newDate = d.toDateString();
 
-async function getDate(url = "") {
-  let response = await fetch(url);
-  let data = await response.json();
-  console.log(data.list);
-  return response;
-}
-getDate(baseURL);
+/* Getting the button and adding an event listener function that is called when the button is clicked. */
+const btn = document.getElementById("generate");
+btn.addEventListener("click", () => {
+  let zip = document.getElementById("zip");
+  let felling = document.getElementById("feelings").value;
+
+  /* Fetching the data from the API. */
+  fetch(baseUrl + zip.value + apiKey)
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      postData("/", data);
+      return data;
+    })
+    /* A function that takes the result of the API call and updates the UI with the data. */
+    .then(function updateUI(result) {
+      let outDate = document.getElementById("date");
+      let outTemp = document.getElementById("temp");
+      let outFeel = document.getElementById("content");
+      outDate.innerText = `date: ${newDate}`;
+      outTemp.innerText = `Tempreture: ${result.main.temp} F`;
+      outFeel.innerText = `My Feellings: ${felling}`;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+// A function that takes a URL and data as input, and returns the response from the server
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
